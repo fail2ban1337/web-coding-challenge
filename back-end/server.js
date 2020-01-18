45175c9bbc4182a120819d3544108d120af02155
Administrator <admin@example.com>
2020-01-18 18:54:22 +0100

    fix formate text

--- back-end/server.js
+++ back-end/server.js
@@ -1,20 +1,19 @@
 const express = require("express");
 
-
 const app = express();
 
 const port = process.env.PORT || 5000;
 
 // Init Middleware
-app.use(express.json({
+app.use(
+  express.json({
     extended: false
-}));
-
+  })
+);
 
 // Routes
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/users", require("./routes/api/users"));
-
-
+app.use("/api/shops", require("./routes/api/shops"));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
